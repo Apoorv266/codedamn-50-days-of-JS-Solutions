5bae2c707391afd3ddf783bdba8c1e3dabc3cdd5
Apoorv <apoorvsharma494@gmail.com>
2022-04-30 00:16:27 +0530

    commit

--- Reverse integer.js
+++ Reverse integer.js
@@ -1,6 +1,6 @@
 //.........................Converting into string first.............................
 
-const num = 1002;
+const num = 3849;
 
 function reverseGivenInteger(num) {
     // write your solution here
@@ -18,5 +18,19 @@ console.log(`Reversed integer is: ${reverseGivenInteger(num)}`)
 
 //.............................By dividing it by 10..........................
 
-// const num = 3849;
-// console.log(num / 10)
+const num2 = 3849;
+const num3 = num2.toString()
+
+reverseGivenInteger2(num2)
+
+function reverseGivenInteger2(num2) {
+ let str = ""
+    for (let i = 0; i <= num2.length-1; i++) {
+    str += num2[i] * 10
+    } 
+    console.log(str)
+}
+
+console.log(num3 % 10)
+
+console.log(`Reversed integer is: ${reverseGivenInteger2(num2)}`)
