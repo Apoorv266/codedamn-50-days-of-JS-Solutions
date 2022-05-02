9ae40bfc24da28da8a927e7d0a46b140413b8940
Apoorv <apoorvsharma494@gmail.com>
2022-05-02 23:55:27 +0530

    commit

---
+++ Remove object from array-challenge 10.js
@@ -0,0 +1,37 @@
+const array = [
+    { field: "id", operator: "eq" },
+    { field: "cStatus", operator: "eq" },
+    { field: "money", operator: "eq" },
+];
+
+const filterField = "money"
+// console.log(array[1].field)
+
+function removeArrayElement(array) {
+    for(let key in array.field) {
+        // for in loop iterates over all the values of key "field"
+        console.log(key)
+    }
+}
+
+console.log(`filtered array: ${removeArrayElement(filterField)}`)
+
+
+//.........................Codedamn's solution.....................
+
+// const array2 = [
+//     { field: "id", operator: "eq" },
+//     { field: "cStatus", operator: "eq" },
+//     { field: "money", operator: "eq" },
+// ];
+
+// const filterField2 = "money"
+
+// function removeArrayElement(filterField) {
+
+//     return array2.filter(function (obj) {
+//             return obj.field !== filterField;
+//         });
+// }
+
+// console.log(removeArrayElement(filterField2))
