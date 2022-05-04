a22154ab34bf06e814757441c1e3a9214a364a2e
Apoorv <apoorvsharma494@gmail.com>
2022-05-04 23:11:31 +0530

    commit

--- Remove object from array-challenge 10.js
+++ Remove object from array-challenge 10.js
@@ -1,3 +1,24 @@
+// const array = [
+//     { field: "id", operator: "eq" },
+//     { field: "cStatus", operator: "eq" },
+//     { field: "money", operator: "eq" },
+// ];
+
+// const filterField = "money"
+// // console.log(array[1].field)
+
+// function removeArrayElement(array) {
+//     for(let key in array.field) {
+//         // for in loop iterates over all the values of key "field"
+//         console.log(key)
+//     }
+// }
+
+// console.log(`filtered array: ${removeArrayElement(filterField)}`)
+
+
+//.........................Codedamn's solution.....................
+
 const array = [
     { field: "id", operator: "eq" },
     { field: "cStatus", operator: "eq" },
@@ -5,34 +26,12 @@ const array = [
 ];
 
 const filterField = "money"
-// console.log(array[1].field)
-
-function removeArrayElement(array) {
-    for(let key in array.field) {
-        // for in loop iterates over all the values of key "field"
-        console.log(key)
-    }
-}
-
-console.log(`filtered array: ${removeArrayElement(filterField)}`)
-
-
-//.........................Codedamn's solution.....................
-
-const array2 = [
-    { field: "id", operator: "eq" },
-    { field: "cStatus", operator: "eq" },
-    { field: "money", operator: "eq" },
-];
-
-const filterField2 = "money"
 
 function removeArrayElement(filterField) {
 
-    return array2.filter(function (obj) {
-            return obj.field !== filterField2;
-            // object whose key value is not equal to filterField2 that is "money" will added to the array2
+    return array.filter(function (obj) {
+            return obj.field !== filterField;
         });
 }
 
-console.log(removeArrayElement(filterField2))
+console.log(`filtered array: ${removeArrayElement(filterField)}`)
---
+++ number to words.js.js
@@ -0,0 +1,8 @@
+let num = 2
+
+const sayNumberInEnglish = (n) => {
+	
+    
+}
+
+console.log(`${num} in english is: ${sayNumberInEnglish(num)}`)
