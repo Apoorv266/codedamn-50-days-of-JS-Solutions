67f0a78a2e634bfdb483e17e6dee934f7386962a
Apoorv <apoorvsharma494@gmail.com>
2022-05-05 23:54:22 +0530

    commit

--- number to words.js.js
+++ number to words.js.js
@@ -1,11 +1,14 @@
-let num = "98"
-let num2 = Math.floor((num % 100) / 10)
-let num3 = num % 10
-// console.log(num2, num3)
+let num = "10"
+let num2 = (Math.floor((num % 100) / 10) * 10).toString()
+let num3 = (num % 10).toString()
+let num4 = num.toString()
+// console.log(typeof num2, typeof num3)
 
-const sayNumberInEnglish = (n, n2) => {
+const sayNumberInEnglish = (n, n2, n3) => {
+    // we will create an array with numbers with values and match it with the user string
     let array = [
         {
+            0: "",
             1: "one",
             2: "two",
             3: "three",
@@ -15,41 +18,58 @@ const sayNumberInEnglish = (n, n2) => {
             7: "seven",
             8: "eight",
             9: "nine",
-            10: "ten"
         },
 
         {
-            2: "twenty",
-            3: "thirty",
-            4: "forty",
-            5: "fifty",
-            6: "sixty",
-            7: "seventy",
-            8: "eighty",
-            9: "ninty",
-            10: "hundred"
+            20: "twenty",
+            30: "thirty",
+            40: "forty",
+            50: "fifty",
+            60: "sixty",
+            70: "seventy",
+            80: "eighty",
+            90: "ninty",
+        },
+
+        {
+            10: "ten",
+            11: "eleven",
+            12: "twelve",
+            13: "thirteen",
+            14: "fourteen",
+            15: "fifteen",
+            16: "sixteen",
+            17: "seventeen",
+            18: "eightteen",
+            19: "nintien"
         }
     ]
 
-    // 
+
     // console.log(Object.keys(array[0]).length)
-    // console.log(n)
-    // console.log(n2)
-    // array[0].hasOwnProperty(key) &&
-
-    // console.log(array[1][9])
-
+    console.log("ten's place :", n)
+    console.log("one's place :", n2)
+    console.log("exceptionals :", n3)
+    
 
     const keys = Object.keys(array[0]);
-    console.log(keys)
+    // console.log(keys)
 
     for (const key in array[0]) {
         for (const key2 in array[1]) {
-            if (n === key && n2 === key2) {
-                return `${array[1][key2]}-${array[0][key]}`;
+            for (const key3 in array[2]) {
+                if (n2 === key && n === key2) {
+                    return `${array[1][key2]}-${array[0][key]}`;
+                }
+
+                else if (n3 === key3){
+                    return `${array[3][key3]}`;
+                }
+            
             }
         }
+
     }
 }
 
-console.log(`${num} in english is: ${sayNumberInEnglish(num2, num3)}`)
+console.log(`${num} in english is: ${sayNumberInEnglish(num2, num3, num4)}`)
