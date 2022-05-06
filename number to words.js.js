c35669337a9692006e4d436e2f2a921203a4bcd6
Apoorv <apoorvsharma494@gmail.com>
2022-05-07 01:53:22 +0530

    commit

--- number to words.js.js
+++ number to words.js.js
@@ -1,10 +1,10 @@
-let num = "10"
+let num = "40"
 let num2 = (Math.floor((num % 100) / 10) * 10).toString() // get tenth place
 let num3 = (num % 10).toString() // one's place
 let num4 = num.toString()
 // console.log(typeof num2, typeof num3)
 
-const sayNumberInEnglish = (n, n2, n3) => {
+const sayNumberInEnglish = (n, n2) => {
 
     // we will create an array with numbers with values and match it with the user string
     // array keys are stored as strings 
@@ -52,27 +52,20 @@ const sayNumberInEnglish = (n, n2, n3) => {
     // console.log(Object.keys(array[0]).length)
     console.log("ten's place :", n)
     console.log("one's place :", n2)
-    console.log("exceptionals :", n3)
+ 
     
 
-    const keys = Object.keys(array[0]);
+    const keys = Object.entries(array[0]);
     // console.log(keys)
 
     for (const key in array[0]) {
         for (const key2 in array[1]) {
-            for (const key3 in array[2]) {
-                if (n2 === key && n === key2) {
-                    return `${array[1][key2]}-${array[0][key]}`;
-                }
-
-                else if (n3 === key3){
-                    return `${array[3][key3]}`;
-                }
-            
-            }
+            if (n2 === key && n === key2) {
+                return n2 != 0 ? `${array[1][key2]}-${array[0][key]}`: `${array[1][key2]}` ;
+            } 
         }
 
     }
 }
 
-console.log(`${num} in english is: ${sayNumberInEnglish(num2, num3, num)}`)
+console.log(`${num} in english is: ${sayNumberInEnglish(num2, num3)}`)
