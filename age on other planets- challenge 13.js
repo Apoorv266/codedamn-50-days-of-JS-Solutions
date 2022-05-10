5891a827124e896f3dee5875e6844d7e1c885892
Apoorv <apoorvsharma494@gmail.com>
2022-05-10 23:32:09 +0530

    commit

--- age on other planets- challenge 13.js
+++ age on other planets- challenge 13.js
@@ -50,3 +50,42 @@ console.log(spaceAge(65965561))
 // REFERENCE : https://museumsvictoria.com.au/media/1869/calculate-your-age-on-other-planets.pdf
 
 // http://girlstart.org/wp-content/uploads/2017/07/13.Age-on-Planets.pdf
+
+
+//......................Codedamn's solution...................................
+
+
+const orbitals = {
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Earth': 1,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132,
+}
+
+const earthYearInSeconds = 31557600
+
+const spaceAge2 = (seconds) => {
+    const yearsInAllPlanets = {
+        'Mercury': 0,
+        'Venus': 0,
+        'Earth': 0,
+        'Mars': 0,
+        'Jupiter': 0,
+        'Saturn': 0,
+        'Uranus': 0,
+        'Neptune': 0
+    }
+
+    const year = seconds / earthYearInSeconds
+
+
+    Object.entries(orbitals).map(([planet, orbitalPeriod]) => {
+        yearsInAllPlanets[planet] = parseFloat((year / orbitalPeriod).toFixed(2))
+    })
+
+    return yearsInAllPlanets
+}
