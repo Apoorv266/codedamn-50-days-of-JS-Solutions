f958b11435bf9967a020fe7a98315776f25b6ed9
Apoorv <apoorvsharma494@gmail.com>
2022-05-11 19:24:38 +0530

    commit

--- check pangram challenge 15.js
+++ check pangram challenge 15.js
@@ -1,12 +1,11 @@
 
 
 
-let input = "W promptly judgd antiqu ivory buckls for th nxt priz"
+let input = "abcdefghijklmnopqrstuvwxyz"
 let input2 = input.toLowerCase()
 let input3 = input2.split("")
 
 const isPangram = (input3) => {
-
     let arr = []
     for (let i = 0; i < input3.length; i++) {
         if (input3[i] != " ") {
@@ -14,19 +13,15 @@ const isPangram = (input3) => {
             bool = true
         }
     }
-   
     let uniqueChars = [...new Set(arr)];
-
     if(uniqueChars.length === 26){
         return true
     }
-
     else{
         return false
     }
        
 }
-
 console.log(isPangram(input3))
 
 // const isPangram = (input) => {
