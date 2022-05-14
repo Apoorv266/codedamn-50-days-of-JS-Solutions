ef7b350133b93a784125f587fd165021b9cf8d59
Apoorv <apoorvsharma494@gmail.com>
2022-05-14 23:51:04 +0530

    commit

--- Ask the bob-challenge 16.js
+++ Ask the bob-challenge 16.js
@@ -1,3 +1,20 @@
 // Sure  -----> "How are you?"
 
 // 'Whoa, chill out!' ----> YELL AT HIM (in all capitals)
+
+//Calm down, I know what I'm doing!' ---> yell a question
+
+// 'Fine. Be that way!' --->  address him without actually saying anything
+
+// 'Whatever'  ---> to anything else
+
+
+function hey(message) {
+let regex = /^[A-Z]*$/
+if (regex.test(message)) {
+    console.log("true")
+}
+
+}
+
+console.log(hey("HOW DARE YOU"))
