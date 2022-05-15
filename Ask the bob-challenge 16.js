0c1dcb2777a4eaa710beafc74aea6a526b25cd39
Apoorv <apoorvsharma494@gmail.com>
2022-05-15 21:12:39 +0530

    commit

--- Ask the bob-challenge 16.js
+++ Ask the bob-challenge 16.js
@@ -10,11 +10,70 @@
 
 
 function hey(message) {
-    let regex = /[A-Z]/g
-    if (message.match(regex)) {
-        console.log("true")
+    let msg = message.split(" ").join("")
+    let regex = /^[A-Z]/g
+    let regex2 = /^[a-z]/g
+    console.log(msg)
+
+
+    if (message.charAt(message.length - 1) === '?' && regex.test(msg)) {
+        return "Calm down, I know what I'm doing!"
     }
 
+    else if ( regex2.test(msg) && message.charAt(message.length - 1) === '?') {
+        return "Sure"
+    }
+
+    else if (regex.test(msg)) {
+        return "Whoa, chill out!"
+    }
+
+    else if (message === "") {
+        return "Fine. Be that way!"
+    }
+
+    else {
+        return "Whatever !"
+    }
 }
 
-console.log(hey("aaa"))
+
+console.log(hey("IUHRWGR SWGI"))
+
+
+
+//........................Codedamn's solution.......................................
+
+const isUpper = (string) => {
+    return !/[a-z]/.test(string) && /[A-Z]/.test(string)
+}
+
+function hey(message) {
+    // Code here
+    let pureLetterString = ""
+    message.split("").forEach(character => {
+        if (/[a-zA-Z]/.test(character)) {
+            pureLetterString += character
+            console.log(pureLetterString)
+        }
+    })
+
+    // console.log(pureLetterString)
+
+    if (isUpper(pureLetterString) && message.trim().charAt(message.length - 1) === '?') {
+        return "Calm down, I know what I'm doing!"
+    }
+    else if (isUpper(pureLetterString)) {
+        return "Whoa, chill out!"
+    }
+    else if (message.trim().charAt(message.length - 1) === '?') {
+        return "Sure."
+    }
+    else if (message === "") {
+        return "Fine. Be that way!"
+    }
+
+    return "Whatever."
+}
+
+console.log(hey('how are you?'))
