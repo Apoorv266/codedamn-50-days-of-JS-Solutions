3cf839508d5b5cfd8935107bf977bd2378a3db99
Apoorv <apoorvsharma494@gmail.com>
2022-05-27 21:23:54 +0530

    commit

--- Longest Consecutive Sequence- challenge 17.js
+++ Longest Consecutive Sequence- challenge 17.js
@@ -1,6 +1,6 @@
 
  let arr = [0,3,7,2,5,8,4,6,0,1,1,3]
-
+// removing the duplicates by using set()
  let arr2 = new Set(arr.sort())
 // sort arrange the digits in array in ascending order
 // to arrange digits in array in ascending order we use arr.sort().reverse()
