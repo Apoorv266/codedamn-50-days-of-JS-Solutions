c13e6e365c440d41de28c3cbff3e77c25d18afe7
Apoorv <apoorvsharma494@gmail.com>
2022-05-29 23:19:12 +0530

    commit

--- Longest Consecutive Sequence- challenge 17.js
+++ Longest Consecutive Sequence- challenge 17.js
@@ -2,12 +2,11 @@
  
  
  let arr = [0,3,7,2,5,8,4,6,0,1,1,3]
-// removing the duplicates by using set()
+
  let arr2 = new Set(arr)
+let arr3 = Array.from(arr2).sort()
 
-let arr3 = Array.from(arr2)
 
- console.log(arr3)
  const longestConsecutiveSequence = (inputArray) => {
 
   for (let i = 0; i < inputArray.length; i++) {
@@ -17,7 +16,7 @@ let arr3 = Array.from(arr2)
      
     }
 
-    console.log(longestConsecutiveSequence(arr2))  // console
+    console.log(longestConsecutiveSequence(arr3))  // console
 
 
     // sort arrange the digits in array in ascending order we use arr.sort()
