b031920f3cc9ee498928fcf0ae9a589a5d4afadd
Apoorv <apoorvsharma494@gmail.com>
2022-05-31 23:36:36 +0530

    commit

--- Longest Consecutive Sequence- challenge 17.js
+++ Longest Consecutive Sequence- challenge 17.js
@@ -1,26 +1,58 @@
 // find longest consecutive sequence
 
 
-let arr = [100, 4, 200, 1, 3, 2, 5]
+let arr = [4,1,3,2,]
 
 let arr2 = new Set(arr)
 let arr3 = Array.from(arr2).sort(function (a, b) { return a - b });
-console.log(arr3[0])
-console.log(arr3[0 - 1] + 1)  // less should be nan
-console.log(arr3[0 + 1] - 1)
+console.log(arr3)
 
-// const longestConsecutiveSequence = (inputArray) => {
+console.log(isNaN(arr3[1 - 1] + 1))
 
-//     for (let i = 0; i < inputArray.length; i++) {
-//         if ((inputArray[i - 1] + 1) == inputArray[i] && inputArray[i] == (inputArray[i + 1] - 1)) {
-//               console.log(inputArray[i])
-//         }
-//     }
-// }
+const longestConsecutiveSequence = (inputArray) => {
 
-// longestConsecutiveSequence(arr3)
+    console.log(inputArray[0])
+    console.log(inputArray[0 - 1])  // less should be nan
+    console.log(inputArray[0])
+
+
+    let arrnew = []
+    for (let i = 0; i < inputArray.length; i++) {
+        if ((inputArray[i - 1] + 1) == inputArray[i] && inputArray[i] == (inputArray[i + 1] - 1)) {
+       arrnew.push(inputArray[i])
+        }
+
+        else if ((inputArray[i - 1]) == undefined){
+            arrnew.push(inputArray[i])
+        }
+
+        else if ((inputArray[i + 1]) !== inputArray[i]){
+            arrnew.push(inputArray[i ])
+        }
+    }
+    console.log(arrnew)
+}
+
+longestConsecutiveSequence(arr3)
 
 
     // sort arrange the digits in array in ascending order we use arr.sort()
 // to arrange digits in array in ascending order we use arr.sort().reverse()
 
+
+
+
+// if (isNaN(inputArray[0 - 1] + 1) === true) {
+            //     arrnew.push(inputArray[i]);
+            // }
+
+            // else if (isNaN(inputArray[0 + 1] - 1) === true){
+            //     arrnew.push(...arrnew, inputArray[i]); 
+            // }
+
+
+
+            //.....................................................................................
+
+
+            
