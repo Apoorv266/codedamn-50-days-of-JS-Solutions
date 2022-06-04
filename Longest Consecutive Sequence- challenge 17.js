bdce66b75f3dc8a0cbfb4fe0dc71d6c01216a7f0
Apoorv <apoorvsharma494@gmail.com>
2022-06-04 23:37:07 +0530

    commit

--- Longest Consecutive Sequence- challenge 17.js
+++ Longest Consecutive Sequence- challenge 17.js
@@ -1,7 +1,7 @@
 // find longest consecutive sequence
 
 
-let arr = [0,3,7,2,5,8,4,6,0,100] 
+let arr = [1, 2, 3, 4, 100] 
 
 let arr2 = new Set(arr)
 let arr3 = Array.from(arr2).sort(function (a, b) { return a - b });
@@ -12,14 +12,14 @@ console.log(isNaN(arr3[1 - 1] + 1))
 const longestConsecutiveSequence = (inputArray) => {
 
     console.log(inputArray[0])
-    console.log(inputArray[0 - 1] + 1)  
-    console.log(inputArray[8 + 1] - 1)
+    console.log(inputArray[0 - 1])  
+    // console.log(inputArray[8 + 1] - 1)
 
 
     let arrnew = []
     for (let i = 0; i < inputArray.length; i++) {
-        
-     if ((inputArray[i - 1]) == undefined){
+
+     if ((inputArray[i - 1]) == undefined && inputArray[i] == (inputArray[i + 1] - 1)){
             arrnew.push(inputArray[i])
         }
 
