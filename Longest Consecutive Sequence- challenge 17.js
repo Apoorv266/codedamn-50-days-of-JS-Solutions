c542c27f15f80a683612fde6f723a02e7cee7cc6
Apoorv <apoorvsharma494@gmail.com>
2022-06-09 17:29:29 +0530

    commit

--- Longest Consecutive Sequence- challenge 17.js
+++ Longest Consecutive Sequence- challenge 17.js
@@ -1,7 +1,7 @@
 // find longest consecutive sequence
 
 
-let arr = [0, 1, 2, 3, 5, 6, 7, 9, 10]
+let arr = [1, 2, 3, 5, 6, 7]
 
 let arr2 = new Set(arr)
 let arr3 = Array.from(arr2).sort(function (a, b) { return a - b });
@@ -32,36 +32,45 @@ const longestConsecutiveSequence = (inputArray) => {
     console.log(arrnew)
 
 
-    // let arr2 = []
-    // for (let i = 0; i < arrnew.length; i++) {
-    //     if ( arrnew[i] != arrnew[i + 1] - 1 ) {
-    //         arr2.push(arrnew[i])
-    //     }
-    // }
-    // console.log(arr2)
+    let arr2 = []
+    for (let i = 0; i < arrnew.length; i++) {
+
+             if (arrnew[i] === 0) {
+            arr2.push([arrnew[0]])
+        }
 
 
+        else if ( arrnew[i] != arrnew[i - 1] + 1 ) {
+            arr2.push([arrnew[i]])
+        }
+    }
+    console.log(arr2)
+
+
+
+    
     var result = []
     for (var i = 0; i < arrnew.length; i++) {
-        if ( arrnew[i] === 0) {
+        if (arrnew[i] === 0) {
             result.push([arrnew[0]])
-        } 
+        }
 
 
         else if (arrnew[i] != arrnew[i - 1] + 1) {
             result.push([arrnew[i]])
-        } 
-        
+        }
+
         else {
+            // console.log(result)
             tmp = result[result.length - 1]
             tmp.push(arrnew[i])
             result[result.length - 1] = tmp
         }
     }
+    
 
-console.log(result)
-
-
+    console.log(result.length)
+    console.log(result)
 
 }
 
@@ -75,3 +84,6 @@ longestConsecutiveSequence(arr3)
 //.....................................................................................
 
 
+let arr4 = [[1], [5] , [7]]
+console.log(arr4.length)
+console.log(arr4[arr4.length - 1])   // [7]
