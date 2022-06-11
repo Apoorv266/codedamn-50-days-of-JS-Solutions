4dad9cf40d838ba3a8d2827cd03bc8c003339eeb
Apoorv <apoorvsharma494@gmail.com>
2022-06-12 03:35:44 +0530

    commit

--- Grains in chessboard - challenge 18.js
+++ Grains in chessboard - challenge 18.js
@@ -1,6 +1,8 @@
 
 let int = 0
 let x = window.prompt("find number of grains on square no : ")
+
+if(x <= 64){
 for (let i = 1; i <= x; i++) {
     if (int === 0) {
      int = 1
@@ -9,4 +11,9 @@ for (let i = 1; i <= x; i++) {
         int = int + int
     }
 }
-console.log(int)
+console.log(BigInt(`${int}`))
+}
+
+else{
+alert("Number should be less than equal to 64")
+}
