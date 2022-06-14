44865406289683fb6aa84e1630c12790f001d574
Apoorv <apoorvsharma494@gmail.com>
2022-06-15 00:14:03 +0530

    commit

--- Resistor Color map- challenge 19.js
+++ Resistor Color map- challenge 19.js
@@ -1,6 +1,6 @@
 
 
-const Colfun = (ColCase) => {
+const colorCode = (color) => {
     let colObj = {
         black: 0,
         brown: 1,
@@ -14,9 +14,10 @@ const Colfun = (ColCase) => {
         white: 9,
     }
 
+
     for (const key in colObj) {
         if (ColCase === key) {
-            console.log(colObj[key])
+            return colObj[key]
         }
     }
 
@@ -25,4 +26,33 @@ const Colfun = (ColCase) => {
 let col = "Blue"
 let ColCase = col.toLowerCase()
 
-Colfun(ColCase)
+console.log(colorCode(ColCase))
+
+//.....................................................................................
+console.log("----------------------------")
+
+const TEST_CASE_1 = 'blue'
+const TEST_CASE_2 = 'white'
+
+const colorCode2 = (color) => {
+    const colorMap = new Map()
+
+    colorMap.set('Black', 0)
+    colorMap.set('Brown', 1)
+    colorMap.set('Red', 2)
+    colorMap.set('Orange', 3)
+    colorMap.set('Yellow', 4)
+    colorMap.set('Green', 5)
+    colorMap.set('Blue', 6)
+    colorMap.set('Violet', 7)
+    colorMap.set('Grey', 8)
+    colorMap.set('White', 9)
+
+    console.log(color.charAt(0).toUpperCase())     
+    console.log(color.substr(1))             
+    console.log(color.charAt(0).toUpperCase() + color.substr(1))
+    return colorMap.get(color.charAt(0).toUpperCase() + color.substr(1))
+}
+
+console.log(colorCode2(TEST_CASE_1))
+console.log(colorCode2(TEST_CASE_2))
