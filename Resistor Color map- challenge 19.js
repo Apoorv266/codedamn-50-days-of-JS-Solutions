2741637e9f9f16dc9d45820cc641c1b085da2763
Apoorv <apoorvsharma494@gmail.com>
2022-06-14 23:44:40 +0530

    commit

--- Resistor Color map- challenge 19.js
+++ Resistor Color map- challenge 19.js
@@ -1,4 +1,23 @@
 
 
+const Colfun = (ColCase) => {
+
+    let colObj = {
+        Black: 0,
+        Brown: 1,
+        Red: 2,
+        Orange: 3,
+        Yellow: 4,
+        Green: 5,
+        Blue: 6,
+        Violet: 7,
+        Grey: 8,
+        White: 9,
+    }
+
+}
+
 let col = "Brown"
 let ColCase = col.toLowerCase()
+
+console.log(ColCase)
