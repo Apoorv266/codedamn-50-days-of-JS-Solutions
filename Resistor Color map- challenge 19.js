cdcb1f8c9ddc73abd6089fbcf2078ece7f5de176
Apoorv <apoorvsharma494@gmail.com>
2022-06-15 00:23:30 +0530

    commit

--- Resistor Color map- challenge 19.js
+++ Resistor Color map- challenge 19.js
@@ -1,17 +1,18 @@
 
 
 const colorCode = (color) => {
+
     let colObj = {
-        black: 0,
-        brown: 1,
-        red: 2,
-        orange: 3,
-        yellow: 4,
-        green: 5,
-        blue: 6,
-        violet: 7,
-        grey: 8,
-        white: 9,
+        Black: 0,
+        Brown: 1,
+        Red: 2,
+        Orange: 3,
+        Yellow: 4,
+        Green: 5,
+        Blue: 6,
+        Violet: 7,
+        Grey: 8,
+        White: 9,
     }
 
 
@@ -23,8 +24,9 @@ const colorCode = (color) => {
 
 }
 
-let col = "Blue"
-let ColCase = col.toLowerCase()
+let col = "red"
+let ColCase = col.charAt(0).toUpperCase() + col.substr(1)   // R + ed = Red
+
 
 console.log(colorCode(ColCase))
 
