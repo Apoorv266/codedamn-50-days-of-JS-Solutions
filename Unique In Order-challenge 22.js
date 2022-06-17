6ab294594460f70c150e04deab5c968ef2da8477
Apoorv <apoorvsharma494@gmail.com>
2022-06-18 01:07:54 +0530

    commit

--- Unique In Order-challenge 22.js
+++ Unique In Order-challenge 22.js
@@ -2,15 +2,22 @@ let uniqueInOrder = (iterable) => {
     //your code here - remember iterable can be a string or an array
     let arrNew = Array.from(iterable)
     console.log(arrNew)
-   
+
+    console.log(typeof arrNew[6])
+    console.log(typeof arrNew[6 + 1])
+
     let newArr = []
     for (let i = 0; i < arrNew.length; i++) {
-     if (arrNew[i] != arrNew[(i + 1)]) {
-        newArr.push(arrNew[i])
-     }
+        if (arrNew[i] === arrNew[i + 1]) {    
+            newArr.push(arrNew[i])
+        }
     }
     console.log(newArr)
 };
-  
 
-uniqueInOrder([1,2,2,3,3])
+
+uniqueInOrder('ABBCcAD')
+
+
+// let a = [1, 2, 3]
+// console.log(indexOf)
