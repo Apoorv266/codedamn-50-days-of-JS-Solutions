0a224245cd820d23b7fb9faa3b811ae069d133cc
Apoorv <apoorvsharma494@gmail.com>
2022-06-17 23:36:58 +0530

    commit

--- Unique In Order-challenge 22.js
+++ Unique In Order-challenge 22.js
@@ -1,5 +1,9 @@
 let uniqueInOrder = (iterable) => {
     //your code here - remember iterable can be a string or an array
+    let set = new set(iterable)
+    console.log(set)
   
 };
   
+
+uniqueInOrder("AABBCC")
