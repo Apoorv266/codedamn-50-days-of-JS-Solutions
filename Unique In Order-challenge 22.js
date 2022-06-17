fecb8791971b8f416fdd9475d388e599691faecb
Apoorv <apoorvsharma494@gmail.com>
2022-06-18 01:28:18 +0530

    commit

--- Unique In Order-challenge 22.js
+++ Unique In Order-challenge 22.js
@@ -8,7 +8,7 @@ let uniqueInOrder = (iterable) => {
 
     let newArr = []
     for (let i = 0; i < arrNew.length; i++) {
-        if (arrNew[i] === arrNew[i + 1]) {    
+        if (arrNew[i] !== arrNew[i + 1]) {   // here we are checking value as well as type, for last element the type of arrNew[i] is string where as type of arrNew[i + 1] would be undefined so arrNew[i] will be pushed to array
             newArr.push(arrNew[i])
         }
     }
@@ -19,5 +19,11 @@ let uniqueInOrder = (iterable) => {
 uniqueInOrder('ABBCcAD')
 
 
-// let a = [1, 2, 3]
-// console.log(indexOf)
+//...................................Codedamn's solution.................................
+
+
+let uniqueInOrder2 = (iterable) => {
+    //your code here - remember iterable can be a string or an array
+  
+    return [...iterable].filter((a, i) => a !== iterable[i-1])   // a is the value of current element and i is the index of current element-----if current element is not equal to its previous element in type and value , it will get filtered to new array
+  };
